325f5e9d5cfeb66adb4e0da6599e42cb26d1aa2d
shivani555 <75723356+shivani555@users.noreply.github.com>
2026-05-15 12:23:36 +0200

    New version

---
+++ can_parser_v2.py
@@ -0,0 +1,8 @@
+class CANparser:
+    def __init__(self, filename):
+        self.filename = filename
+        self.results = []
+        self.pass_count = 0
+        self.fail_count = 0 
+
+        
